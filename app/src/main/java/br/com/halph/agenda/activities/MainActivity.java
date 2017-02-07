bcac1fdbbbc7a97a4f6ad0eb2b8c959d46965aec
Fernando Mota <fm.aken@gmail.com>
2017-02-07 18:58:13 -0200

    Primeiro commit do projeto

---
+++ app/src/main/java/br/com/halph/agenda/activities/MainActivity.java
@@ -0,0 +1,21 @@
+package br.com.halph.agenda.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.halph.agenda.R;
+
+
+/**
+ * Created by Android on 06/02/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
